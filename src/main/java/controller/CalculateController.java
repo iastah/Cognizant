530157ce64aec15f60c4980358f23027b6b864c6
Allen Tsai <Allen.Tsai@walmart.com>
2020-03-02 12:27:15 -0600

    Calculate

---
+++ src/main/java/controller/CalculateController.java
@@ -0,0 +1,15 @@
+package controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/calories/calculate")
+public class CalculateController {
+
+	@GetMapping("ping")
+	public String ping() {
+		return "pong";
+	}
+}
